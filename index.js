8c3f57a7fea7203fe58a572dacacac0c5f805064
Kirby Campbell <jkirbycampbell@gmail.com>
2018-10-26 19:15:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 $(document).ready(function (){
 });
+
+var displayError = () => $('#errors').html("I'm sorry, there's been an error. Please try again.")
+
+var renderCommit = (commit) => {
+  return `<li><h3>${commit.sha}</h3><p>${commit.commit.message}</p></li>`
+}
+
+var renderCommits = (data) => {
+  let result = data.map((commit)=>renderCommit(commit)).join('')
+  return `<ul>${result}</ul>`
+}
