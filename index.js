8ba25ae8a47562e3f767f6c2a10d2f93f1840068
Kirby Campbell <jkirbycampbell@gmail.com>
2018-10-26 19:16:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,3 +32,12 @@ var renderSearchResult = (result) => {
 }
 
 var renderSearchResults = (data) => data.items.map( result => renderSearchResult(result))
+
+var searchRepositories = () => {
+  const searchTerms = $('#searchTerms').val()
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
+      $('#results').html(renderSearchResults(data))
+    }).fail(error => {
+      displayError()
+    })
+}
